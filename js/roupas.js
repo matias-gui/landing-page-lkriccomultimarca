72610574e8a8732ebdf5effbf3a68ca73ee5f5a5
Guilherme Matias <guimatias890@gmail.com>
2026-01-20 20:55:48 -0300

    feat: consumindo api e mostrando na tela

--- js/roupas.js
+++ js/roupas.js
@@ -1,11 +1,24 @@
-const url = "https://fakestoreapi.com/products/category/men's clothing"
-
+const url = "https://fakestoreapi.com/products/category/men's%20clothing"
+let carregarRoupasmasc = document.querySelector('.carregarRoupasmasc')
 async function chamarApi() {
     const res = await fetch(url);
 
+    
+
     if(res.status === 200){
         const apires = await res.json();
-        console.log(apires)
+        apires.forEach(element  => {
+            carregarRoupasmasc.innerHTML += `
+             <article class="cardroupas">
+                <img src=" ${element.image}" alt="" class="imgcard">
+                <p class="titulo"> ${element.title}</p>
+                <p class="preco">${element.price}</p>
+            </article>
+            `
+            
+        });
+        
+
     }
 }
 
