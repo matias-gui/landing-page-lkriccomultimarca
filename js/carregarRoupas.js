acfc60fe6e6e0b508ba864ffd1a513b40d6c915c
Guilherme Matias <guimatias890@gmail.com>
2026-03-02 21:20:36 -0300

    style: animação carrossel na seção roupas

--- js/carregarRoupas.js
+++ js/carregarRoupas.js
@@ -1,16 +1,37 @@
 let carregarRoupas = document.querySelector('.carregarroupas')
-
 fetch('/json/roupamasc.json') // Faz a aquisição do json
 .then(response => response.json())
 .then(converserJson => {
-    converserJson.roupasMasc.forEach(element => {
+    try{
+      converserJson.roupasMasc.forEach(element => {
         carregarRoupas.innerHTML += `
-        <article class="containercarregarroupas">
-        <img src="${element.img}" alt="" class ="carregarroupasimg"/>
-        <h2 class="carregarroupastitulo">${element.titulo}</h2>
-        <p class="carregarroupapreco">${element.preco}</p>
-        <p class="carregarroupacor">${element.cor}</p>
-        <article/>
+        <article class = "containercarregarroupas">
+            <img src = "${element.img}">
+            <h3>${element.titulo}</h3>
+            <p>${element.preco}</p>
+            <p>${element.cor}</p>
+        </article>
         `
-    });
-})
+        } 
+    )} catch(erro){
+        console.error(erro);
+    }
+    setInterval(carrossel, 3000);
+});
+
+let count = 0;
+function carrossel(){
+ let containercarregarroupas = document.querySelectorAll(".containercarregarroupas");
+
+    if(count >= containercarregarroupas.length -1){
+        count = 0;
+    }else{
+        count++ ;
+    }
+
+    moverCarrossel();
+}
+
+function moverCarrossel(){
+    carregarRoupas.style.transform =  `translateX(-${count * 100}%)`;
+}
