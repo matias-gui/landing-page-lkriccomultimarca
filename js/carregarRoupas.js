654ca2ccc92184e5708f1064827258d6192c28d8
Guilherme Matias <guimatias890@gmail.com>
2026-03-05 21:22:29 -0300

    feat: carregando carrossel conforme o button é acionado

--- js/carregarRoupas.js
+++ js/carregarRoupas.js
@@ -1,23 +1,46 @@
 let carregarRoupas = document.querySelector('.carregarroupas')
+
+function carregarRoupasMasc(){
 fetch('/json/roupamasc.json') // Faz a aquisição do json
 .then(response => response.json())
 .then(converserJson => {
     try{
       converserJson.roupasMasc.forEach(element => {
-        carregarRoupas.innerHTML += `
+        carregarRoupas.innerHTML +=
+
+        `
         <article class = "containercarregarroupas">
-            <img src = "${element.img}">
+            <img src="${element.img}" class = "carregarImg"/>
             <h3>${element.titulo}</h3>
-            <p>${element.preco}</p>
-            <p>${element.cor}</p>
         </article>
         `
         } 
     )} catch(erro){
         console.error(erro);
     }
-    setInterval(carrossel, 3000);
+    setInterval(carrossel, 4000);
 });
+};
+function carregarRoupasFemin(){
+    fetch('/json/roupamasc.json')
+    .then(response2 => response2.json())
+    .then(converserJson2 => {
+        try{
+            converserJson2.roupasFem.forEach(element => {
+                carregarRoupas.innerHTML += `
+                <article class="containercarregarroupas"> 
+                <img src="${element.img}" class= "carregarImg"/>
+                <h3>${element.titulo}</h3>
+                </article>
+                `
+            })
+        } catch (erro){
+            console.error(erro)
+        };
+        setInterval(carrossel, 4000)
+    });
+};
+
 
 let count = 0;
 function carrossel(){
--- js/script.js
+++ js/script.js
@@ -65,15 +65,17 @@ escreverh2()
 let buttonmasc = document.querySelector('#buttonmasc')
 let buttonfemin = document.querySelector('#buttonfemin')
 let carregarroupas = document.querySelector('.carregarroupas')
-buttonmasc.addEventListener('click', () => {
-     buttonmasc.classList.add("active")
-     buttonfemin.classList.remove("active")
-   
+buttonmasc.addEventListener('click', () => {    
+     buttonmasc.classList.add("active");
+     buttonfemin.classList.remove("active");
+     carregarroupas.innerHTML = carregarRoupasMasc()
 })
 
 buttonfemin.addEventListener('click', () => {
-   buttonmasc.classList.remove("active")
-   buttonfemin.classList.add("active")
+   buttonmasc.classList.remove("active");
+   buttonfemin.classList.add("active");
+   carregarroupas.innerHTML = carregarRoupasFemin();
+
    
 })
 
