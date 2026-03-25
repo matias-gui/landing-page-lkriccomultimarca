ab46e5800f8d4e81a08af6ff0404416f870eeaf3
Guilherme Matias <guimatias890@gmail.com>
2026-03-24 21:44:57 -0300

    feat: carrossel funcional com controle manual

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -3,36 +3,43 @@ const buttonAnterior = document.querySelector('.buttonAnterior');
 const buttonProximo = document.querySelector('.buttonProximo');
 
 
-function carrossel(){
-  const mostrarProducts = document.querySelectorAll('.mostrarProducts');
+function carrossel() {
+    const mostrarProducts = document.querySelectorAll('.mostrarProducts');
+    // Carregar o primeiro elemento
+    mostrarProducts[0].classList.add('ativo');
 
-  if(mostrarProducts.length > 0){
-     mostrarProducts[1].classList.add('ativo');
+    let index = 0;
+    // Quando o button proximo é clicado, acontece um evento.
+    buttonProximo.addEventListener('click', () => {
+        //Remove a classe ativo
+        mostrarProducts[index].classList.remove('ativo');
+        // Encrementação do index
+        index++
+        // Se o index passar do tamanho do mostrarProducts, ele volta para o inicio
+        if (index >= mostrarProducts.length) {
+            index = 0;
+        };
+        // Adicionar a classe ativo para o proximo elemento
+        mostrarProducts[index].classList.add('ativo');
+    });
 
-    let ativo = document.querySelectorAll('.ativo')
-    console.log(ativo)
-  }
-   
+    // Quando o button anterior é clicado, acontece outro evento.
+    buttonAnterior.addEventListener('click', () => {
+        // Remove a classe ativo
+        mostrarProducts[index].classList.remove('ativo')
+        // Decrementação do index
+        index--
+        // Se o index for menor que 0, ou seja, se ele for menor que o tamanho do mostrarProducts, o index vai receber o ultimo elemento
+        if (index < 0) {
+            index = mostrarProducts.length - 1;
+        }
+        // Adicionando a classe ativo no elemento
+        mostrarProducts[index].classList.add('ativo')
+        console.log(index)
+    });
+
 };
 
 
-let index = 0;
 
-// Quando o button proximo é clicado, acontece um evento.
- buttonProximo.addEventListener('click', ()=> {
-    if(index >= 5){
-        index = 0;
-    }
-    index++
-    console.log(index)
-})
-
-// Quando o button anterior é clicado, acontece outro evento.
-buttonAnterior.addEventListener('click', ()=> {
-    if(index <= 0){
-        index = 5;
-    }
-    index--
-    console.log(index)
-})
 
