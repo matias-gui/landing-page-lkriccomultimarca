328daba8aa032eafb4025d0c5bc243f1f26f1124
Guilherme Matias <guimatias890@gmail.com>
2026-03-25 22:15:08 -0300

    feat: carregamento dos produtos, conforme os buttons são clicados

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,10 @@ let buttonClose = document.querySelector('.icone-fechar');
 let menu = document.querySelector('.menu-navegacao');
 
 // Menu
-buttonMenu.addEventListener('click', function () {
+buttonMenu.addEventListener('click',  () => {
     menu.style.clipPath = 'circle(160% at 100% 0%)';
     menu.style.transition = '1s all';
-    
+
 
 
     main.style.transition = '0.8s ease-in-out';
@@ -15,8 +15,8 @@ buttonMenu.addEventListener('click', function () {
     buttonMenu.style.transition = '1.2s ease-in-out';
 });
 
-buttonClose.addEventListener('click', function () {
-    menu.style.clipPath ='circle(0% at 100% 0%)';
+buttonClose.addEventListener('click', () => {
+    menu.style.clipPath = 'circle(0% at 100% 0%)';
     main.style.transition = '0.8s ease-in-out';
     buttonMenu.style.opacity = '1';
     buttonMenu.style.transition = '1.2s ease-in-out';
@@ -25,18 +25,34 @@ buttonClose.addEventListener('click', function () {
 
 // Carregar Pagina Novidades
 let buttonnovidades = document.querySelector('.buttonnovidades');
-buttonnovidades.addEventListener('click' , () => {
+buttonnovidades.addEventListener('click', () => {
     window.location.href = '/pages/roupasnovidades.html'
 });
 
 // Carregar Roupas Masculina ou Feminina
 let buttonmasc = document.querySelector('#buttonmasc');
 let buttonfemin = document.querySelector('#buttonfemin');
-buttonmasc.addEventListener('click', () => {    
-     buttonmasc.classList.add("active");
+
+// Carrega os produtos quando a página é carregada
+document.addEventListener('DOMContentLoaded', () => {
+    carregarMasc()
+})
+buttonmasc.addEventListener('click', () => {
+    
+    if(buttonmasc.classList.contains("active")) return
      buttonfemin.classList.remove("active");
+    buttonmasc.classList.add("active")
+
+    carregarMasc()
+   
+   
 });
 buttonfemin.addEventListener('click', () => {
-   buttonmasc.classList.remove("active");
-   buttonfemin.classList.add("active");
+    if(buttonfemin.classList.contains("active")) return
+    buttonmasc.classList.remove("active");
+    buttonfemin.classList.add("active");
+   
+    carregarFem()
+
+
 });
