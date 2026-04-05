885e35b7ee7cd24a179b66132081ce588b9a5a6c
Guilherme Matias <guimatias890@gmail.com>
2026-04-05 19:25:13 -0300

    feat: criação da pagina Carrinho,implementação do  button carrinho

--- assets/js/carrinho.js
+++ assets/js/carrinho.js
@@ -1,11 +1,11 @@
 const carrinho = document.querySelector('.iconeCarrinho')
 const buttonCarrinho = document.querySelector('.buttonCarrinho')
 
+
 carrinho.addEventListener('click', () =>{
+    window.location.href = '/pages/carrinho.html';
     console.log('clicou')
 })
 
-buttonCarrinho.addEventListener("click", ()=>{
-    console.log('clicou')
-})
+
 
--- assets/js/pageProducts.js
+++ assets/js/pageProducts.js
@@ -1,12 +1,11 @@
 //window.location.search => pega tudo que vem depois do ? na URL
 //new URLSearchParams() => ferramenta pra ler dados da URL
 const params = new URLSearchParams(window.location.search);
-console.log(params)
-    const id = params.get('id');
 
 fetch('/assets/data/produtos.json')
 .then(res => res.json())
 .then( dados => {
+  const id = params.get('id');
    const produto = dados.roupasMasc.find( element => element.id == id);
    let imgProduto = document.querySelector('.imgProduto')
    let nomeProduto = document.querySelector('.nomeProduto')
