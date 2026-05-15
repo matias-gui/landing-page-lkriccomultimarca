5b4ca1745dec9a3b2471aff1504ec76529d9317e
Guilherme Matias <guimatias890@gmail.com>
2026-05-14 21:42:33 -0300

    fix: corrige persistência do carrinho no localStorage

--- assets/js/adicionarAoCarrinho.js
+++ assets/js/adicionarAoCarrinho.js
@@ -3,23 +3,30 @@ let carrinhoItems = JSON.parse(localStorage.getItem('carrinhoItems')) || [];
 let buttonCarrinho = document.querySelector('.buttonCarrinho')
 
 function adicionarProduto(item){
-     if(!carrinho) return;
-     carrinhoItems.push(item);
-     localStorage.setItem('carrinho', JSON.stringify(carrinhoItems));
+     if(!carrinhoItems) return;
+     let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+     carrinho.push(item);
+     localStorage.setItem('carrinho', JSON.stringify(carrinho));
      console.log('Produto adicionado ao carrinho:', item);
+    // window.location.href = '/pages/carrinho.html'
   
 }
 
+
 if (buttonCarrinho){     
-     buttonCarrinho.addEventListener('click', () =>{
+     buttonCarrinho.addEventListener('click', async () =>{
      const params = new URLSearchParams(window.location.search);
+     const response = await fetch('/assets/data/produtos.json')
+     const dados = await response.json()
+
+     const todosProdutos = [
+          ...dados.roupasMasc,
+          ...dados.roupasFem
+     ]
      const id = Number(params.get('id'));
-     adicionarProduto(id) 
+     const produto = todosProdutos.find((p) => p.id == id)
+     adicionarProduto(produto);
      })
 }
-
-
-//buttonCarrinho.addEventListener('click', () => {
-  //   window.location.href = '/pages/carrinho.html'
-//})
+     
 
